70a11ffa2b20b5223292df438cba5312c311adcc
irfaan1610 <kirfaanahamed1610@gmail.com>
2026-04-06 16:00:36 +0530

    connect frontend to deployed backend

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8080/api";
+const API_BASE = "https://finflow-vpm9.onrender.com/api";
 
 async function apiFetch(endpoint, options = {}) {
     const token = localStorage.getItem("token");
